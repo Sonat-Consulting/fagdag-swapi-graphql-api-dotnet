5afb19d250c528ee1a82828273b5a80af2eadac0
Martin Iden Leithaug <martin.leithaug@gmail.com>
2021-08-24 20:04:39 +0200

    Expand vehicleType with more fields

--- StarWarsApi/Models/Vehicle.cs
+++ StarWarsApi/Models/Vehicle.cs
@@ -3,8 +3,16 @@ namespace StarWarsApi.Models
     public class Vehicle
     {
         public string Name { get; set; }
-
         public string Model { get; set; }
-
+        public string CargoCapacity { get; set; }
+        public string Consumables { get; set; }
+        public string CostInCredits { get; set; }
+        public string Created { get; set; }
+        public string Crew { get; set; }
+        public string Length { get; set; }
+        public string Manufacturer { get; set; }
+        public string MaxAtmospheringSpeed { get; set; }
+        public string Passengers { get; set; }
+        public string VehicleClass { get; set; }
     }
 }
--- StarWarsApi/SchemaDefinition/Types/VehicleType.cs
+++ StarWarsApi/SchemaDefinition/Types/VehicleType.cs
@@ -9,6 +9,16 @@ namespace StarWarsApi.SchemaDefinition.Types
         {
             Field(t => t.Name).Description("The name of this vehicle");
             Field(t => t.Model).Description("The model or official name of this vehicle");
+            Field(t => t.CargoCapacity).Description("The maximum number of kilograms that this starship can transport");
+            Field(t => t.Consumables).Description("The maximum length of time that this vehicle can provide consumables for its entire crew without having to resupply");
+            Field(t => t.CostInCredits).Description("The cost of this starship new, in galactic credits");
+            Field(t => t.Created).Description("he ISO 8601 date format of the time that this resource was created.");
+            Field(t => t.Crew).Description("The number of personnel needed to run or pilot this vehicle");
+            Field(t => t.MaxAtmospheringSpeed).Description("The maximum speed of this vehicle in the atmosphere");
+            Field(t => t.CargoCapacity).Description("The maximum number of kilograms that this vehicle can transport");
+            Field(t => t.Passengers).Description("The number of non-essential people this vehicle can transport");
+            Field(t => t.Length).Description("The length of this vehicle in meters");
+            Field(t => t.Manufacturer).Description("The manufacturer of this vehicle. Comma separated if more than one");
         }
     }
 }
